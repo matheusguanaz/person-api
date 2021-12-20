93f2d0f4efa952c8db7e83fd4fa74d66b9732970
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-12-20 18:59:01 -0300

    Creating Repository for person

---
+++ src/main/java/one/digitalinnovation/personApi/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.personApi.repository;
+
+import one.digitalinnovation.personApi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+}
