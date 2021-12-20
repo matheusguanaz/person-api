61ca9acf28933ecd5a3bb003ce6fd7ce596328f8
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-12-20 18:59:55 -0300

    Creating method post for entity person

--- src/main/java/one/digitalinnovation/personApi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personApi/controller/PersonController.java
@@ -1,17 +1,30 @@
 package one.digitalinnovation.personApi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personApi.dto.MessageResponseDTO;
+import one.digitalinnovation.personApi.entity.Person;
+import one.digitalinnovation.personApi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String helloMessage(){
-        return "Hello Digital Innovation One";
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with id " + person.getId())
+                .build();
     }
 }
 
