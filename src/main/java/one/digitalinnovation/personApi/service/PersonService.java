9afaab60dcb283c03c9886ad664a93ef9055c110
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-12-22 13:04:06 -0300

    developed list all methods

--- src/main/java/one/digitalinnovation/personApi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personApi/service/PersonService.java
@@ -8,6 +8,9 @@ import one.digitalinnovation.personApi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
@@ -26,5 +29,12 @@ public class PersonService {
                 .builder()
                 .message("Created person with id " + personToSave.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll(){
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
