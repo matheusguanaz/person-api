7c339e7f74cb24d2eae598b5c13b24249986a8d0
Matheus Guanaz <matheusguanaz@hotmail.com>
2021-12-22 14:16:48 -0300

    developed delete person by id

--- src/main/java/one/digitalinnovation/personApi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personApi/service/PersonService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -41,7 +40,16 @@ public class PersonService {
     }
 
     public PersonDTO getById(Long id) throws PersonNotFoundException {
-        Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+        Person person = verifyIfExists(id);
         return personMapper.toDTO(person);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        verifyIfExists(id);
+        personRepository.deleteById(id);
     }
 }
